d5f4b9ed4c3dc202fe9fe892fababd01132f6f55
Miguel Mario <miguelmario.mf@gmail.com>
2022-07-06 12:14:17 +0200

    Nueva funcionalidad calcular_cumpleaños añadida

--- file_2.py
+++ file_2.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 
+
 from file_1 import hola_mundo
+import datetime
 
 def saludar(nombre):
     
     lista=hola_mundo()
-    cad=""
-    for d in lista:
-        cad=cad+" "+d
-    return f'{cad} {nombre}'
+    resultado=" ".join(lista)+" "+nombre
+    return resultado
 
 print(saludar("Lucas"))
+
+def calcular_cumpleaños(fecha):
+    hoy=datetime.datetime.now().strftime('%d/%m/%Y')
+    hoy=hoy.split("/")
+    fecha=fecha.split("/")
+    resultado=f'Para tu {abs(int(hoy[2])-int(fecha[2]))} cumpleaños quedan {abs(int(hoy[1])-int(fecha[1]))} meses y {abs(int(hoy[0])-int(fecha[0]))} dias'
+    return resultado
+    
+
+print(calcular_cumpleaños("02/07/1999"))
